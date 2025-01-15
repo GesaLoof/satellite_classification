10de5cb6d036687bea196f379701fbad5fc1b47e
GesaLoof <gesaloof@gmail.com>
2025-01-15 17:08:33 +0100

    added function backbone

--- src/satellite_classification/_satellite_classification.py
+++ src/satellite_classification/_satellite_classification.py
@@ -1,3 +1,6 @@
+from subprocess import call
+
+
 """
 You should write your module/set of function/class(es) here
 """
@@ -7,3 +10,32 @@ class satelliteclassification:
     def __init__(self, arg):
         super(satelliteclassification, self).__init__()
         self.arg = arg
+
+
+def data_loader(path_to_data):
+    """
+    Function that loads the data that we want to classify
+    Args: 
+        path_to_data (str): path to data that is supposed to be classified
+
+
+
+    """
+    
+    return
+
+"""
+step1: download satellite images
+using https://github.com/andolg/satellite-imagery-downloader/tree/main
+tbd: zoom level
+where to host the images (g drive?)
+
+
+
+step2: pre-processing of sat images
+step3: downloading training data
+step4: pre-processing training data
+step5: training classifier
+step6: classifying data
+
+"""
